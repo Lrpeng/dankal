91e74f94b6d92dea10f84f1dfc2e230303c7ff71
Lrpeng <Lrp@luorupengdeMini.lan>
2015-08-01 19:35:34 +0800

    Mac
    Mac test

--- libs/Controller/dankalController.php
+++ libs/Controller/dankalController.php
@@ -2,7 +2,11 @@
 		header("Content-type:text/html;charset=utf-8");
 		require_once('../Model/dankalModel.php');
 		
-		mysql_connect('localhost','root','');
+		if(mysql_connect('localhost','root','')){
+			echo "数据库连接成功";
+		}else{
+			echo "数据库连接失败";
+		}
 		mysql_select_db('dankal');
 		mysql_query("set names 'utf8'");
 		
@@ -15,26 +19,11 @@
 		$longnum = trim($_POST['longnum']);
 		$shortnum = trim($_POST['shortnum']);
 		
-		echo $username; 
-		
 		$sql = "insert into account( username , password , head , longnum , shortnum) values('$username','$password','none','$longnum','$shortnum')";
  
    		mysql_query( $sql );
 		
 		$uid = mysql_insert_id();
 		echo $uid;
-		
-		/*mysql_connect('localhost', 'root', '');
-		mysql_select_db('dankal');
-		mysql_query("set names 'utf8'");
-		//已知的数据变量有
-		$name = '李sssss四';
-		$age = 'sd';
-		//在这里进行数据查询
-		$sql = "insert into test(a, b) values('$name', '$age')";
-		mysql_query($sql); //执行插入语句
-		$uid = mysql_insert_id();
-		echo $uid;*/
-		
 		
 ?>
