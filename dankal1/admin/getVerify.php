0e5db3d3cfcc4be9ef7087c77ea3d46819ede1cd
Linzy <Arclin@linzhiyudeMini.lan>
2015-08-10 16:46:37 +0800

    test Commit
    commit

--- dankal1/admin/getVerify.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * 获取随机验证码
- */
-	require_once '../include.php';
-	verifyImage(1,4,50,2);
-	
-?>
--- dankal1/admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once '../include.php';
-	session_destroy();
-	setcookie("username","",time()-1);
-	setcookie("account_id","",time()-1);	
-	echo "<script>window.location.href='login.php';</script>";
-?>
